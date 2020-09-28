14b3b62977487fa5e45884da6d5a23d67cb728d6
meanz1 <zndnfjqbd1206@gmail.com>
2020-09-28 20:43:18 +0900

    camera

--- src/calibration/include/calibration/calibration.hpp
+++ src/calibration/include/calibration/calibration.hpp
@@ -2,6 +2,7 @@
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
 #include <iostream>
+#include <string>
 #include <stdio.h>
 #include <math.h>
 #include <vector>
@@ -11,14 +12,38 @@
 class calibration
 {
     public:
-        cv::Mat r_src, l_src;        
+        cv::Mat src;        
+        cv::Mat rvec, tvec; //rotation & translation vectors
 
-
-        calibration()
+        struct Internal_Cam_Param
         {
-            r_src = cv::Mat::zeros(480, 640, CV_8UC3);
-            l_src = cv::Mat::zeros(480, 640, CV_8UC3);
-            //runloop();
+            double fx = 496.321719;
+            double fy = 495.920276;
+            double cx = 314.523890;
+            double cy = 236.862583;
+            double k1 = 0;
+            double k2 = 0;
+            double p1 = 0;
+            double p2 = 0;
+        };
+        Internal_Cam_Param Inter_cam;
+
+
+        double cameraMatrix[9] = {
+            Inter_cam.fx, 0, Inter_cam.cx, 0, Inter_cam.fy, Inter_cam.cy, 0, 0, 1
+        };
+
+        double distCoeffs[4] = {
+            Inter_cam.k1, Inter_cam.k2, Inter_cam.p1, Inter_cam.p2
+        };
+        
+        int img_rows, img_cols;
+        calibration()
+        {            
+            src = cv::Mat::zeros(480, 640, CV_8UC3);
+            img_rows = src.rows;
+            img_cols = src.cols;
+            
             videocapture();
         }
         ~calibration()
@@ -26,5 +51,6 @@ class calibration
 
         }
         int videocapture();
+        void SolvePnP();
 
 };
--- src/calibration/src/calibration.cpp
+++ src/calibration/src/calibration.cpp
@@ -3,43 +3,87 @@
 
 int calibration::videocapture()
 {
-    cv::VideoCapture cap(1);
-    cv::VideoCapture cap2(0);
+    cv::VideoCapture cap(0);
 
-    if(!cap.isOpened() && !cap2.isOpened())
+    if(!cap.isOpened())
     {
         std::cout << "Capture Couldn`t open. " << std::endl;
         return 0;
     }
 
-    cv::namedWindow("right_Video");
-    cv::namedWindow("left_Video");
+    cv::namedWindow("Video");
     // cv::namedWindow("flip_right_Video");
     // cv::namedWindow("flip_left_Video");
 	while(ros::ok())
 	{
-	    cap >> r_src;
-        cap2 >> l_src;
-        if(r_src.empty() || l_src.empty())
+	    cap >> src;
+        if(src.empty())
         {
             std::cout << "Video over!" << std::endl;
             break;
         }
-        cv::Mat flip_r = cv::Mat::zeros(480, 640, CV_8UC3);
-        cv::Mat flip_l = cv::Mat::zeros(480, 640, CV_8UC3);
+        cv::Mat flip = cv::Mat::zeros(480, 640, CV_8UC3);
 
-        cv::flip(r_src, flip_r, 1);
-        cv::flip(l_src, flip_l, 1);        
-        cv::swap(r_src, flip_r);
-        cv::swap(l_src, flip_l);
+        cv::flip(src, flip, 1);
+        cv::swap(src, flip);
         
-        cv::imshow("right_Video", r_src);
-        cv::imshow("left_Video", l_src);
-        // cv::imshow("flip_right_Video", flip_r);
-        // cv::imshow("flip_left_Video", flip_l);
+        cv::imshow("Video", src);
        
         cv::waitKey(1);
-        printf("hhhh\n");
     }
 }
 
+void calibration::SolvePnP()
+{
+    std::vector<cv::Point3f> objectPoints;  //3d world coordinates
+    std::vector<cv::Point2f> imagePoints;   //2d world coordinates
+
+    if(imagePoints.size() == 0)
+        return;
+    
+    //createObjPoints:
+    // for(int j = 0; j < img_rows; j++)
+    //     for(int k = 0; k < img_cols; k++)
+    //         objectPoints.push_back(cv::Point3f(float(k * square_size), float(j * square_size ), 0));
+    // extract rotation & translation matrix
+
+
+    std::vector<calibration::cameraMatrix> Cam_vec;
+    std::vector<calibration::distCoeffs> Cam_Dist_vec;
+    
+    Cam_vec.clear();
+    Cam_Dist_vec.clear();
+    Cam_vec.push_back(cameraMatrix);
+    Cam_Dist_vec.push_back(distCoeffs);
+
+    struct Cam_param
+    {
+        //camera parameters
+        cv::Mat CameraMatrix_Mat(3, 3, CV_64FC1, Cam_vec);   //Camera Matrix
+        // distortion 외곡
+        cv::Mat DistCoeffs_Mat(4, 1, CV_64FC1, distCoeffs);
+    };
+
+
+    Cam_param CameraParam;
+    
+    cv::solvePnP(
+        objectPoints,
+        imagePoints,
+        CameraParam.CameraMatrix_Mat,
+        CameraParam.DistCoeffs_Mat,
+        rvec, tvec);
+
+    std::cout << "rvec\n" << rvec << std::endl;
+    std::cout << "tvec\n" << tvec << std::endl;
+
+
+    // cv::Mat R;
+    // Rodrigues(rvec, R);
+    // cv::Mat R_inv = R.inv();
+    // cv::Mat P = -R_inv*tvec;
+    // double* p = (double *)P.data;
+
+    // // camera position
+    // printf("x=%lf, y=%lf, z=%lf", p[0], p[1], p[2]);   
+}
